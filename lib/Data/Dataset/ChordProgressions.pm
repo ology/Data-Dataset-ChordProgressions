66aa4fd3f4f05bc2e4b263fa7f058f41ad763d1e
Gene Boggs <gene@ology.net>
2021-05-13 07:41:46 -0700

    Re-wrap

--- lib/Data/Dataset/ChordProgressions.pm
+++ lib/Data/Dataset/ChordProgressions.pm
@@ -37,9 +37,9 @@ L<Music::Chord::Note> (listed in the source of that module).
 There are a few odd chord "progressions" like
 C<"Eb7-Eb7-Eb7-Eb7","III-III-III-III">.  Strange...
 
-I stumbled across this list, saved it on my harddrive for a long time,
-and then forgot where it came from!  Also the documentation in this
-list said nothing about who made it or how.
+I stumbled across this list, saved it on my hard-drive for a long
+time, and then forgot where it came from!  Also the documentation in
+this list said nothing about who made it or how.
 
 =cut
 
