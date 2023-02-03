447314a110ce8c5357cd42bce0ed45dda41d0cb2
Gene Boggs <gene@ology.net>
2023-02-02 20:42:20 -0800

    Bump version

--- lib/Data/Dataset/ChordProgressions.pm
+++ lib/Data/Dataset/ChordProgressions.pm
@@ -2,7 +2,7 @@ package Data::Dataset::ChordProgressions;
 
 # ABSTRACT: Provide access to hundreds of possible chord progressions
 
-our $VERSION = '0.0200';
+our $VERSION = '0.0201';
 
 use strict;
 use warnings;
