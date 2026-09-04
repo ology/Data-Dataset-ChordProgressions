b89e5d562880569154d2a550905873e12f93cae2
Gene Boggs <gene.boggs@gmail.com>
2026-09-04 11:58:25 -0700

    Bump

--- lib/Music/Dataset/ChordProgressions.pm
+++ lib/Music/Dataset/ChordProgressions.pm
@@ -2,7 +2,7 @@ package Music::Dataset::ChordProgressions;
 
 # ABSTRACT: Provide access to hundreds of chord progressions
 
-our $VERSION = '0.0401';
+our $VERSION = '0.0500';
 
 use strict;
 use warnings;
